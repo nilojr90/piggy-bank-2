b66c3ff7657c4b9a2aaae632262ad571059c8eed
nilojr90 <nilojr90@gmail.com>
2020-07-18 23:37:56 -0300

    GET Transaction

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,14 +1,38 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
 // import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
+
+interface Balance{
+  income: number,
+  outcome: number,
+  total: number
+};
+
+interface Response{
+  transactions: Transaction[],
+  balance: Balance
+}
+
+
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const balance = transactionsRepository.getBalance();
+
+  const transactions = transactionsRepository.find();
+
+  return response.json({
+    transactions,
+    balance
+  });
 });
 
 transactionsRouter.post('/', async (request, response) => {
