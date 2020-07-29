3116c34cf8f3193d7bc9d39d68ac98c5b4d299e7
codefactor-io <support@codefactor.io>
2020-07-29 16:02:55 +0000

    [CodeFactor] Apply fixes

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -5,48 +5,51 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   ManyToOne,
-  JoinColumn
-} from 'typeorm';
+  JoinColumn,
+} from "typeorm";
 
-import Category from './Category';
+import Category from "./Category";
 
-@Entity('transactions')
+@Entity("transactions")
 class Transaction {
-
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
   @Column()
   title: string;
 
   @Column()
-  type: 'income' | 'outcome';
+  type: "income" | "outcome";
 
-  @Column({type:'decimal', precision:10, scale:2})
+  @Column({ type: "decimal", precision: 10, scale: 2 })
   value: string;
 
   @ManyToOne(() => Category)
-  @JoinColumn({name:'category_id'})
+  @JoinColumn({ name: "category_id" })
   category: Category;
 
-  @CreateDateColumn({ type: 'timestamp with time zone', default: () => 'CURRENT_TIMESTAMP' })
+  @CreateDateColumn({
+    type: "timestamp with time zone",
+    default: () => "CURRENT_TIMESTAMP",
+  })
   created_at: Date;
 
-  @UpdateDateColumn({ type: 'timestamp with time zone', default: () => 'CURRENT_TIMESTAMP' })
+  @UpdateDateColumn({
+    type: "timestamp with time zone",
+    default: () => "CURRENT_TIMESTAMP",
+  })
   updated_at: Date;
 
-  public getValue(): number{
-    if(this.value == undefined){
+  public getValue(): number {
+    if (this.value == undefined) {
       return 0.0;
     }
     return parseFloat(this.value);
   }
 
-  public setValue(newValue:number): void{
+  public setValue(newValue: number): void {
     this.value = newValue.toString();
   }
 }
-
-
 
 export default Transaction;
