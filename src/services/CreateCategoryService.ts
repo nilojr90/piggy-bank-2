bd8d0fd79fd36abeaacc7b34d10012623de678fb
codefactor-io <support@codefactor.io>
2020-07-29 16:23:20 +0000

    [CodeFactor] Apply fixes

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,23 +1,18 @@
-import AppError from '../errors/AppError';
+import { getRepository } from "typeorm";
+import AppError from "../errors/AppError";
 
-import { getRepository } from 'typeorm';
-import Category from '../models/Category';
-
+import Category from "../models/Category";
 
 class CreateCategoryService {
   public async execute(title: string): Promise<Category> {
-
     const categoryRepository = getRepository(Category);
-    let categoryExist = await categoryRepository.findOne(
-      { title }
-    );
+    let categoryExist = await categoryRepository.findOne({ title });
 
     if (!categoryExist) {
       const newCategory = categoryRepository.create({
-        title
+        title,
       });
-      await categoryRepository.save(newCategory)
-      .catch(() =>{
+      await categoryRepository.save(newCategory).catch(() => {
         throw new AppError("Falha ao salvar categoria", 500);
       });
       categoryExist = newCategory;
