abeaa65cc9b4d20060e4da2eb31c3c489ed97ebe
codefactor-io <support@codefactor.io>
2020-07-29 16:01:28 +0000

    [CodeFactor] Apply fixes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import 'reflect-metadata';
-import 'dotenv/config';
+import "reflect-metadata";
+import "dotenv/config";
 
-import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 
-import routes from './routes';
-import AppError from './errors/AppError';
+import routes from "./routes";
+import AppError from "./errors/AppError";
 
-import createConnection from './database';
+import createConnection from "./database";
 
 createConnection();
 const app = express();
@@ -18,7 +18,7 @@ app.use(routes);
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
-      status: 'error',
+      status: "error",
       message: err.message,
     });
   }
@@ -26,8 +26,8 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   console.error(err);
 
   return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
+    status: "error",
+    message: "Internal server error",
   });
 });
 
