65f3347d1496dbae2822c7a55bf8de2e68511437
codefactor-io <support@codefactor.io>
2020-07-29 16:00:33 +0000

    [CodeFactor] Apply fixes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import transactionsRouter from './transactions.routes';
+import transactionsRouter from "./transactions.routes";
 
 const routes = Router();
 
-routes.use('/transactions', transactionsRouter);
+routes.use("/transactions", transactionsRouter);
 
-routes.get('/', async (request, response) => {
-  return response.json({"server":"Piggy Bank","version":"0.2.0"});
+routes.get("/", async (request, response) => {
+  return response.json({ server: "Piggy Bank", version: "0.2.0" });
 });
 
 export default routes;
